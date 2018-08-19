ca6186dec14c5c769d950ff6b3df857c8693b883
Marco Aring <contato@marcoaring.com.br>
2018-08-18 23:47:03 -0300

    Added login integration with WP.

---
+++ front-page.php
@@ -0,0 +1,26 @@
+<?php 
+if(is_user_logged_in()){
+	get_header(); 
+?>
+
+	<main class="main-content">
+		<div class="row main-content__series" v-if="home.Response">
+			<div class="col s12 m6" v-for="(item, index) in home.Search">
+				<div class="card">
+					<div class="card-image">
+						<img :src="[item.Poster != 'N/A' ? item.Poster : '<?php echo get_template_directory_uri(); ?>/assets/img/no-picture.jpg']" class="responsive-img">
+						<a :href="'catalog/' + item.imdbID" class="btn-floating btn-large halfway-fab waves-effect waves-light blue">
+							<i class="material-icons">add</i>
+						</a>
+					</div>
+					<div class="card-content">
+						<span class="card-title">{{item.Title}}</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+<?php 
+	get_footer(); 
+}
+?>
